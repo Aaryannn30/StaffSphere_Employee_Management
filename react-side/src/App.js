ca9eac1762e467b65f7d18ae1e188272e3f4fb07
Aryan Maru <aryanmaru1230@gmail.com>
2024-07-23 23:13:49 +0530

    first commit

---
+++ react-side/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    details: []
+  }
+
+  componentDidMount() {
+    let data;
+    axios.get(`http://127.0.0.1:8000/`)
+      .then(res => {
+        data = res.data;
+        this.setState({ details: data });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          Django Generated Data :
+        </header>
+        <hr></hr>
+        {this.state.details.map((output, id) => (
+          <div key={id}>
+            <div>
+              <h2>
+                {output.employee}
+              </h2>
+              <h3>
+                {output.department}
+              </h3>
+            </div>
+          </div>
+        )
+        )}
+      </div>
+    )
+  }
+}
+
+// export default App;
