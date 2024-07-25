b0793c045d08fdfa72a15cd512682888830c005c
Aryan Maru <aryanmaru1230@gmail.com>
2024-07-25 21:47:54 +0530

    DB Connection ✅

---
+++ express-backend/index.js
@@ -0,0 +1,16 @@
+// const disconnectToMongo = require('./db');
+const connectTOMongo = require('./db');
+const express = require('express')
+
+connectTOMongo();
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
